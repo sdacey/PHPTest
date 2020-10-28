c7913a79e1c7e8f3d9112c38708a95b9a8e40895
sdacey <sdacey@unitec.ac.nz>
2020-10-28 13:16:08 +1300

    Simon

---
+++ Test05.php
@@ -0,0 +1,11 @@
+<form>
+    <p>
+        Width: <input type="text" name = "width">
+    </p>
+    <p>
+        Height: <input type="text" name = "height">
+    </p>
+    <p>
+        <input type="submit" name="submit" id="submit" value="Submit">
+    </p>
+</form>
