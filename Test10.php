a25d4ca5b03b5c357a74c445ce9302443c777ca3
sdacey <sdacey@unitec.ac.nz>
2020-10-28 21:05:39 +1300

    Simon

--- Test10.php
+++ Test10.php
@@ -1,15 +1,11 @@
 <?php
-echo strlen("Hello world!<br>"); // outputs 12
-?>
-<?php
-echo str_word_count("Hello world!<br>"); // outputs 2
-?>
-<?php
-echo strrev("Hello world!<br>"); // outputs !dlrow olleH
-?>
-<?php
-echo strpos("Hello world!", "world")."<br>"; // outputs 6
-?>
-<?php
-echo str_replace("world", "Dolly", "Hello world!")."<br>"; // outputs Hello Dolly!
+echo strlen("Hello world!"); // outputs 12
+echo ("<br");
+echo str_word_count("Hello world!"); // outputs 2
+echo ("<br");
+echo strrev("Hello world!"); // outputs !dlrow olleH
+echo ("<br");
+echo strpos("Hello world!", "world"); // outputs 6
+echo ("<br");
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
 ?>
