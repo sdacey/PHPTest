feb457b1210c6cd4f65af8640c4b11e4c7dd4760
sdacey <sdacey@unitec.ac.nz>
2020-10-29 11:34:59 +1300

    Simon

--- Test10.php
+++ Test10.php
@@ -1,19 +1,11 @@
 <?php
 echo strlen("Hello world!"); // outputs 12
-echo (" <br");
-?>
-<?php
+echo (" <br>");
 echo str_word_count("Hello world!"); // outputs 2
-echo (" <br");
-?>
-<?php
+echo (" <br>");
 echo strrev("Hello world!"); // outputs !dlrow olleH
-echo (" <br");
-?>
-<?php
+echo (" <br>");
 echo strpos("Hello world!", "world"); // outputs 6
-echo (" <br");
-?>
-<?php
+echo (" <br>");
 echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
 ?>
