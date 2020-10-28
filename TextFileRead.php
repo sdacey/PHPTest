8d36bcfad331c9bdcc9c125f4c7c3b841307d6c4
sdacey <sdacey@unitec.ac.nz>
2020-10-29 12:43:28 +1300

    Simon

--- TextFileRead.php
+++ TextFileRead.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h1>Read File</h1>";
-$file=fopen(‘TestTextFile.txt’,’r’);
+$file=fopen('TestTextFile.txt','r');
 //Output a line of the file until the end is reached
 while ( !feof( $file ) ) {
     echo fgets( $file ) . "<br />";
