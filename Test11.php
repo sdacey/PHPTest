2efb3d1a6ac140668637cf60795ec7cad2be8c58
sdacey <sdacey@unitec.ac.nz>
2020-10-30 15:31:37 +1300

    simon

--- Test11.php
+++ Test11.php
@@ -1,5 +1,6 @@
 <?php
-$cars = array("Volvo", "BMW", "Toyota", "Mercedes");
+$cars = array("Volvo", "BMW", "Toyota", "Mercedes", "Audi");
+$cars.sort();
 foreach ($cars as $value) {
     echo "I like " . $value." <br>";
         }
