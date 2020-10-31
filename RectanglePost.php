970db84866525614ea06dbf235e84485cf3556a1
sdacey <sdacey@unitec.ac.nz>
2020-10-31 20:38:13 +1300

    simon

---
+++ RectanglePost.php
@@ -0,0 +1,11 @@
+<form action="calculatePost.php" method="POST">
+    <p>
+        Width: <input type="text" name = "width">
+    </p>
+    <p>
+        Height: <input type="text" name = "height">
+    </p>
+    <p>
+        <input type="submit" name="submit" id="submit" value="Submit">
+    </p>
+</form>
